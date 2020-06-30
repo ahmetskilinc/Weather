738c010b6aac6dbca8b25d94aca422e843008fae
Ahmet Kilinc <akx9@icloud.com>
2020-06-30 15:27:32 +0100

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Ahmet Kilinc on 30/06/2020.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
